b3ca7090e3b741c7d67b8bdb85e6fb7126c725a4
Leafuke <zhuolin.yew@outlook.com>
2026-01-13 15:15:25 +0800

    feat: 版本号迭代至1.11.7

--- MineBackup/MineBackup.cpp
+++ MineBackup/MineBackup.cpp
@@ -33,7 +33,7 @@ GLFWwindow* wc = nullptr;
 static map<wstring, GLuint> g_worldIconTextures;
 static map<wstring, ImVec2> g_worldIconDimensions;
 static vector<int> worldIconWidths, worldIconHeights;
-string CURRENT_VERSION = "1.11.5";
+string CURRENT_VERSION = "1.11.7";
 atomic<bool> g_UpdateCheckDone(false);
 atomic<bool> g_NewVersionAvailable(false);
 atomic<bool> g_NoticeCheckDone(false);
