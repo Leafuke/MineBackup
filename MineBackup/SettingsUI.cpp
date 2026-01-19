3d70b1437e2483680ee39d28385e7d4a5e3533ec
Leafuke <zhuolin.yew@outlook.com>
2026-01-19 21:10:08 +0800

    fix: 修复Linux上编译报错

--- MineBackup/SettingsUI.cpp
+++ MineBackup/SettingsUI.cpp
@@ -19,7 +19,6 @@
 
 using namespace std;
 
-// 外部变量声明
 extern bool showSettings;
 extern bool specialSetting;
 extern bool isSilence;
@@ -41,8 +40,7 @@ extern vector<wstring> restoreWhitelist;
 extern const char* lang_codes[2];
 extern const char* langs[2];
 
-// 前向声明
-void ApplyTheme(const int& theme);
+inline void ApplyTheme(const int& theme);
 wstring SelectFolderDialog();
 wstring SelectFileDialog();
 void ReStartApplication();
